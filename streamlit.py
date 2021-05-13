a81675c8057f9b60d411a09742ba8531ed438a5f
Stacey McLennan-Waldal <stacey.waldal@gmail.com>
2021-05-13 15:21:54 -0600

    Updated code

--- streamlit.py
+++ streamlit.py
@@ -8,3 +8,15 @@ import pandas as pd
 # Hello!
 You did it :)
 """
+def recommend_similar(prodid):
+    similarprods = sortedcoocurrences[prodid]
+    top5 = similarprods[:5]
+    top5 = [(id, idtoname[id][0]) for id, _ in top5]
+    print("You may also like:")
+    for id, name in top5:
+        print(name,id)
+    return top5
+  
+  prodid = input("Enter product_id:")
+  
+  print(prodid)
